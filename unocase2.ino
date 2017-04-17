bf4cabc3ac88c4f20c86d85906c302870643a098
ApoorvaEliza <johnapoorvaeliza@gmail.com>
2017-04-17 20:46:07 +0530

    Add files via upload

---
+++ unocase2.ino
@@ -0,0 +1,41 @@
+#include <SoftwareSerial.h>
+
+const int pin = 12;
+const int pin2 = 13;
+const int pin3 = 11;
+const int pin4 = 10;
+SoftwareSerial esp(7,6);
+
+void setup() {
+Serial.begin(9600);
+esp.begin(9600);
+pinMode(pin,OUTPUT);
+pinMode(pin2,OUTPUT);
+}
+
+void loop() {
+if(esp.available()){  
+String line = esp.readString();
+Serial.print(line);
+
+if(line=="ON1"){
+  digitalWrite(pin, HIGH);
+  Serial.println("LED1 is ON");
+}
+if(line=="OFF1"){
+  digitalWrite(pin, LOW);
+    Serial.println("LED1 is OFF");
+
+}
+
+if(line=="ON0"){
+  digitalWrite(pin2, HIGH);
+    Serial.println("LED2 is ON");
+
+}
+if(line=="OFF0"){
+  digitalWrite(pin2,LOW);
+    Serial.println("LED2 is OFF");
+
+}
+}}
